d287f994ff6f704820ecf77ccd049a3e2caba333
davemins <davemins4125@naver.com>
2021-10-08 22:05:33 +0900

    01-104

---
+++ Chapter 01/01-104.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int money, pay;
+
+    cout<<"판매 금액을 만원 단위로 입력(-1 to end): "
+    cin>>money;
+
+    pay=50+(money*0.12);
+
+    while (1)
+    {
+    
+        {
+            /* code */
+        }
+        
+        if(money=-1);
+            cout<<"프로그램을 종류합니다."
+            break;
+
+        else
+            cout<<"이번 달 급여: "<<pay;
+    }
+
+    return 0;
+    
+}
